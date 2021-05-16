15e6cf78e19e6eebe0d1e0652c697612c535a2ba
Joseph Perez <perezj0230@gmail.com>
2021-05-15 23:42:41 -0400

    added navbar and dark/light mode

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Hello from "./components/Hello";
+import Navbar from "./components/Navbar";
+import { useState } from "react";
 
 function App() {
+  const [words, setWords] = useState(["hello", "goodbye", "joshisagay"]);
+  const [darkMode, setDarkMode] = useState(true);
+
+  const changeWords = () => {
+    setWords(["bonjour", ...words]);
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div className="App">
+    <div className={("App", darkMode ? "dark" : "light")}>
+      <Navbar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={changeWords}>arist</button>
+        {words.map((word) => {
+          return (
+            <div>
+              <Hello msg={word} />
+              <h4>test</h4>
+            </div>
+          );
+        })}
+        {/* {[<h1>Gang</h1>, <h1>Chains</h1>, <h1>Drugs</h1>]} */}
+        <h1>Gang</h1>
+        <h1>Chains</h1>
+        <h1>Drugs</h1>
       </header>
     </div>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className="Navbar-container">
+      <div className="row">
+        <h1 className="title">Ranking Game</h1>
+        <h1 className="links">Home</h1>
+        <h1 className="links">Results</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
